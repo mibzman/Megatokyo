d76a2e65bc7e4933168e9ada1f828d4415c7b648
mibzmam <27thisotope@gmail.com>
2016-09-16 15:47:56 -0400

    Version 1.2ish

--- Megatokyo Comic Viewer/src/com/isotope/Megatokyo_Comic_Viewer/MyActivity.java
+++ Megatokyo Comic Viewer/src/com/isotope/Megatokyo_Comic_Viewer/MyActivity.java
@@ -7,6 +7,7 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.ConnectivityManager;
@@ -15,68 +16,112 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.util.Log;
+import android.support.v4.util.LruCache;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.*;
+import com.google.ads.AdRequest;
+import com.google.ads.AdView;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import uk.co.senab.photoview.PhotoViewAttacher;
 
+
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Random;
 
+
 public class MyActivity extends Activity {
     final Context context = this;
     int currentcomic;
     int latestComic;
+    String comicString;
+    String totalTitle;
     ImageDownloader loadthisURL = new ImageDownloader();
     SharedPreferences Megatokyo_data;
+    public static String filename = "first";
+    public static String filename1 = "second";
+    private AdView adView;
+    private LruCache<String, Bitmap> mMemoryCache;
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        Megatokyo_data = getSharedPreferences("First_File", 0);
+        Megatokyo_data = getSharedPreferences("Latest", 0);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        //FirstImageDownloader startup = new FirstImageDownloader();
-        //startup.execute();
         if (checkNet()){
-        try{
-            Document doc = Jsoup.connect("http://megatokyo.com/").get();
-            Elements spans = doc.select("span#strip-bl");
-            Element mySpan = spans.first();
-            Element image = mySpan.child(0);
-            final String cakeString= image.attr("src");
-            String shortenedString = cakeString.substring(7,11);
-            currentcomic = Integer.parseInt(shortenedString);
-            latestComic = currentcomic;
-            ImageDownloader startup = new ImageDownloader();
-            startup.execute(currentcomic);
-            final Button first = (Button) findViewById(R.id.first);
-            final Button prev = (Button) findViewById(R.id.prev);
-            final Button rand = (Button) findViewById(R.id.rand);
-            final Button next = (Button) findViewById(R.id.next);
-            final Button last = (Button) findViewById(R.id.last);
-            first.setEnabled(false);
-            prev.setEnabled(false);
-            rand.setEnabled(false);
-            next.setEnabled(false);
-            last.setEnabled(false);
-
-        }catch(Exception e){
-            Log.e("Image", "Failed to load image", e);
-        }
-        }else{
-                nopeNet();
+            try{
+                Document doc = Jsoup.connect("http://megatokyo.com/").get();
+                Elements spans = doc.select("span#strip-bl");
+                Element mySpan = spans.first();
+                Element image = mySpan.child(0);
+                final String cakeString= image.attr("src");
+                if (cakeString.length() > 0){
+                   // Toast.makeText(getApplicationContext(), "off", Toast.LENGTH_SHORT).show();
+                }
+                String shortenedString = cakeString.substring(7,11);
+                currentcomic = Integer.parseInt(shortenedString);
+                latestComic = currentcomic;
+                comicString = Integer.toString(currentcomic);
+                ImageDownloader startup = new ImageDownloader();
+                startup.execute(currentcomic);
+                final Button first = (Button) findViewById(R.id.first);
+                final Button prev = (Button) findViewById(R.id.prev);
+                final Button rand = (Button) findViewById(R.id.rand);
+                final Button next = (Button) findViewById(R.id.next);
+                final Button last = (Button) findViewById(R.id.last);
+                first.setEnabled(false);
+                prev.setEnabled(false);
+                rand.setEnabled(false);
+                next.setEnabled(false);
+                last.setEnabled(false);
+            }catch(Exception e){
+                Log.e("Image", "Failed to load image", e);
             }
+        }else{
+            nopeNet();
+        }
         buttonlistener();
+        AdView adView = (AdView)this.findViewById(R.id.adView);
+        adView.loadAd(new AdRequest());
+        // Get max available VM memory, exceeding this amount will throw an
+        // OutOfMemory exception. Stored in kilobytes as LruCache takes an
+        // int in its constructor.
+        final int maxMemory = (int) (Runtime.getRuntime().maxMemory() / 1024);
+
+        // Use 1/8th of the available memory for this memory cache.
+        final int cacheSize = maxMemory / 8;
+
+        mMemoryCache = new LruCache<String, Bitmap>(cacheSize) {
+            @Override
+            protected int sizeOf(String key, Bitmap bitmap) {
+                // The cache size will be measured in kilobytes rather than
+                // number of items.
+                return  (bitmap.getRowBytes() * bitmap.getHeight()) / 1024;
+            }
+        };
     }
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        TextView view = (TextView) findViewById(R.id.textView);
+        String restoredTitle = Megatokyo_data.getString(Integer.toString(currentcomic), "An Error Occured");
 
-
+        view.setText(restoredTitle);
+    }
+    public void addBitmapToMemoryCache(String key, Bitmap bitmap) {
+        if (getBitmapFromMemCache(key) == null) {
+            mMemoryCache.put(key, bitmap);
+        }
+    }
+    public Bitmap getBitmapFromMemCache(String key) {
+        return mMemoryCache.get(key);
+    }
     public void buttonlistener() {
         final Button first = (Button) findViewById(R.id.first);
         final Button prev = (Button) findViewById(R.id.prev);
@@ -89,6 +134,7 @@ public class MyActivity extends Activity {
             public void onClick(View v) {
                 if (checkNet()) {
                     currentcomic = 1;
+                    String comicString = Integer.toString(currentcomic);
                     prev.setEnabled(false);
                     rand.setEnabled(false);
                     next.setEnabled(false);
@@ -97,7 +143,6 @@ public class MyActivity extends Activity {
                     loadAncomic.execute(currentcomic);
                 }else{
                     nopeNet();
-                  //  rand.setText("cake");
                 }
             }
         });
@@ -106,6 +151,7 @@ public class MyActivity extends Activity {
             public void onClick(View v) {
                 if (checkNet()) {
                     currentcomic--;
+                    String comicString = Integer.toString(currentcomic);
                     first.setEnabled(false);
                     prev.setEnabled(false);
                     rand.setEnabled(false);
@@ -125,6 +171,7 @@ public class MyActivity extends Activity {
                 if (checkNet()) {
                     Random randComic = new Random();
                     currentcomic = randComic.nextInt(latestComic);
+                    String comicString = Integer.toString(currentcomic);
                     first.setEnabled(false);
                     prev.setEnabled(false);
                     rand.setEnabled(false);
@@ -143,6 +190,7 @@ public class MyActivity extends Activity {
             public void onClick(View v) {
                 if (checkNet()) {
                     currentcomic++;
+                    String comicString = Integer.toString(currentcomic);
                     first.setEnabled(false);
                     prev.setEnabled(false);
                     rand.setEnabled(false);
@@ -161,6 +209,7 @@ public class MyActivity extends Activity {
             public void onClick(View v) {
                 if (checkNet()) {
                     currentcomic = latestComic;
+                    String comicString = Integer.toString(currentcomic);
                      first.setEnabled(false);
                     prev.setEnabled(false);
                     rand.setEnabled(false);
@@ -175,14 +224,11 @@ public class MyActivity extends Activity {
             }
         });
     }
-
     public boolean checkNet() {
         ConnectivityManager cn=(ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo nf=cn.getActiveNetworkInfo();
         if(nf == null)
         {
-            Button rand = (Button) findViewById(R.id.rand);
-            //rand.setText("null");
             return false;
         } else if(nf.isConnected()){
             return true;
@@ -192,7 +238,6 @@ public class MyActivity extends Activity {
             return false;
         }
     }
-
     class ImageDownloader
             extends AsyncTask<Integer, Integer, String> {
         protected void onPreExecute(){
@@ -203,26 +248,48 @@ public class MyActivity extends Activity {
         protected String doInBackground(Integer... params) {
             //TODO Auto-generated method stub
             try{
+                Megatokyo_data = getSharedPreferences(filename1, 0);
                 int number = params[0];
-                Document doc = Jsoup.connect("http://megatokyo.com/strip/"+ number).get();
+                String key = Integer.toString(number);
+                String result = Megatokyo_data.getString(key, "default");
+                if (result.equals("default")) {
+                    Document doc = Jsoup.connect("http://megatokyo.com/strip/"+ number).get();
+                    Elements titleDivs = doc.select("div#comic");  //start of title getter
+                    Element myDivs = titleDivs.first();
+                    Element myDiv = myDivs.child(0);
+                    Element theDiv = myDiv.child(1);
+                    String title = theDiv.ownText();    //end title getter
+                    SharedPreferences.Editor editor = Megatokyo_data.edit();
+                    editor.putString(key, title);
+                    editor.commit();
+                    return title;
+                }else{
+               /* Document doc = Jsoup.connect("http://megatokyo.com/strip/"+ number).get();
+
                 Elements titleDivs = doc.select("div#comic");  //start of title getter
                 Element myDivs = titleDivs.first();
                 Element myDiv = myDivs.child(0);
                 Element theDiv = myDiv.child(1);
-                String title = theDiv.ownText();    //end title getter
+                String title = theDiv.ownText();    //end title getter */
+                final String title = Megatokyo_data.getString(key, "default");
                 return title;
+                }
             }catch(Exception e){
                 Log.e("Image", "Failed to load image", e);
-
+                Toast.makeText(getApplicationContext(), "An Error Occured", Toast.LENGTH_LONG).show();
             }
             return null;
         }
         protected void onProgressUpdate(Integer... params){
 
         }
-        protected void onPostExecute(String title){
+        protected void onPostExecute( final String title){
            TextView durrrr = (TextView) findViewById(R.id.textView);
             durrrr.setText(title);
+            totalTitle = title;
+            SharedPreferences.Editor editor = Megatokyo_data.edit();
+            editor.putString(Integer.toString(currentcomic), totalTitle);
+            editor.commit();
            ImageDownloaderCont tim = new ImageDownloaderCont();
             tim.execute(currentcomic);
         }
@@ -244,32 +311,52 @@ public class MyActivity extends Activity {
         protected Bitmap doInBackground(Integer... params) {
             //TODO Auto-generated method stub
             try{
+                Megatokyo_data = getSharedPreferences(filename, 0);
                 int number = params[0];
-                Document doc = Jsoup.connect("http://megatokyo.com/strip/"+ number).get();
-                Elements spans = doc.select("span#strip-bl");
-                Element mySpan = spans.first();
-                Element image = mySpan.child(0);
-                final String srcString = image.attr("src");;
-                URL url = new URL("http://megatokyo.com/"+ srcString);
-                HttpURLConnection httpCon =
-                        (HttpURLConnection)url.openConnection();
-                if(httpCon.getResponseCode() != 200)
-                    throw new Exception("Failed to connect");
-                InputStream is = httpCon.getInputStream();
-                return BitmapFactory.decodeStream(is);
+                String key = Integer.toString(number);
+                String result = Megatokyo_data.getString(key, "default");
+                if (result.equals("default")) {
+                    Document doc = Jsoup.connect("http://megatokyo.com/strip/"+ number).get();
+                    Elements spans = doc.select("span#strip-bl");
+                    Element mySpan = spans.first();
+                    Element image = mySpan.child(0);
+                    final String srcString = image.attr("src");
+                    SharedPreferences.Editor editor = Megatokyo_data.edit();
+                    editor.putString(key, srcString);
+                    editor.commit();
+                    URL url = new URL("http://megatokyo.com/"+ srcString);
+                    HttpURLConnection httpCon =
+                            (HttpURLConnection)url.openConnection();
+                    if(httpCon.getResponseCode() != 200)
+                        throw new Exception("Failed to connect");
+                    InputStream is = httpCon.getInputStream();
+                    return BitmapFactory.decodeStream(is);
+                }  else{
+                    final String srcString = Megatokyo_data.getString(key, "default");
+                    URL url = new URL("http://megatokyo.com/"+ srcString);
+                    HttpURLConnection httpCon =
+                            (HttpURLConnection)url.openConnection();
+                    if(httpCon.getResponseCode() != 200)
+                        throw new Exception("Failed to connect");
+                    InputStream is = httpCon.getInputStream();
+                    return BitmapFactory.decodeStream(is);
+                }
             }catch(Exception e){
                 Log.e("Image", "Failed to load image", e);
+                Toast.makeText(getApplicationContext(), "off", Toast.LENGTH_SHORT).show();
             }
             return null;
         }
         protected void onProgressUpdate(Integer... params){
 
         }
-        protected void onPostExecute(Bitmap img){
+        protected void onPostExecute(final Bitmap img){
             ImageView iv = (ImageView) findViewById(R.id.imageView);
             if(iv!=null && img!=null){
                 iv.setImageBitmap(img);
                 new PhotoViewAttacher(iv);
+                addBitmapToMemoryCache(Integer.toString(currentcomic), img);
+
             }
             closeDialog();
             enablebuttons();
@@ -282,6 +369,12 @@ public class MyActivity extends Activity {
             oopsDialog();
         }
     }
+  /*  @Override
+    public void onResume() {
+        super.onResume();
+        ImageView view = (ImageView) findViewById(R.id.imageView);
+        //view.setImageBitmap(img);
+    }                                  */
     public void setComicTitle(String title){
         TextView titleText = (TextView) findViewById(R.id.textView);
         titleText.setText(title);
@@ -355,6 +448,7 @@ public class MyActivity extends Activity {
                         int pants = Integer.parseInt(jumpCode.getText().toString());
                         if (pants <= latestComic) {
                             currentcomic = pants;
+                            String comicString = Integer.toString(currentcomic);
                             hurrDeDerp.execute(currentcomic);
                               }
                     }
@@ -413,6 +507,16 @@ public class MyActivity extends Activity {
                 return super.onOptionsItemSelected(item);
         }
     }
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
 
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            setContentView(R.layout.main);
+
+        } else {
+            setContentView(R.layout.main);
+        }
+    }
 }
 
--- Megatokyo Comic Viewer/src/com/isotope/Megatokyo_Comic_Viewer/fragment.java
+++
@@ -1,9 +0,0 @@
-package com.isotope.Megatokyo_Comic_Viewer;
-
-import android.app.Fragment;
-
-/**
- * Created by Sam on 2/27/14.
- */
-public class fragment extends Fragment {
-}
